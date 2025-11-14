812a54a96c88a6a6edae0bcd0dbe48f414a142db
jânio <janiotechn@gmail.com>
2025-11-14 16:41:17 -0300

    test: test Birth and Phone

--- src/test/java/tech/janio/taskflow/domain/UserTest.java
+++ src/test/java/tech/janio/taskflow/domain/UserTest.java
@@ -2,22 +2,36 @@ package tech.janio.taskflow.domain;
 
 import com.janiotech.taskflow.domain.entities.User;
 import com.janiotech.taskflow.domain.valueobjects.BirthDate;
+import com.janiotech.taskflow.domain.valueobjects.Phone;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Domain Entity User Test")
 public class UserTest {
     @Test
     public void BirthDateUserTest(){
         User user = new User();
-        LocalDate dateTest = LocalDate.of(2012,2,2);
-        user.setBirth(new BirthDate(dateTest));
-        assertNotNull(dateTest);
+        BirthDate dateTest = new BirthDate(LocalDate.of(2012,2,3));
+        user.setBirth(dateTest);
+        assertEquals(2012, user.getBirth().getYear());
+        assertEquals(2, user.getBirth().getMonthValue());
+        assertEquals(3, user.getBirth().getDayOfMonth());
         assertEquals(13, user.getBirth().getAge());
+        assertEquals("03/02/2012", user.getBirth().toString());
+        assertThrows(IllegalArgumentException.class, () -> user.setBirth(new BirthDate(null)));
+        assertThrows(IllegalArgumentException.class, () -> user.setBirth(new BirthDate(LocalDate.of(2013,2,3))));
+    }
+
+    @Test
+    public void PhoneUserTest(){
+        User user = new User();
+        Phone phoneUser = new Phone("+55 (79) 9 9817 - 5138");
+        user.setPhone(phoneUser);
+        assertNotNull(phoneUser);
+        assertEquals("+5579998175138", user.getPhone().toString());
     }
 }
