93d644d52689cae4e921edadc2c7e40699a9e609
jânio <janiotechn@gmail.com>
2025-11-23 11:32:57 -0300

    feat: added methods findAllPaginated and countAll interface of contract

--- src/main/java/com/janiotech/taskflow/domain/repositories/UserRepository.java
+++ src/main/java/com/janiotech/taskflow/domain/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.janiotech.taskflow.domain.repositories;
 
 import com.janiotech.taskflow.domain.entities.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ public interface UserRepository {
     Optional<User> findById(UUID id);
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    List<User> findAllPaginated(int page, int size);
+    long countAll();
 }
